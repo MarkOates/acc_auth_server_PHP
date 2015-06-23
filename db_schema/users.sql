52da99f71f8793fcb06e42e687cfafefaaa31ac5
Mark Oates <euph_boy@hotmail.com>
2015-06-23 17:11:01 -0400

    Cleaned up users.sql file

--- db_schema/users.sql
+++ db_schema/users.sql
@@ -1,30 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 3.5.1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Jun 23, 2015 at 08:56 AM
--- Server version: 5.5.24-log
--- PHP Version: 5.3.13
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `acc_login_auth`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
 
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -33,16 +6,11 @@ CREATE TABLE IF NOT EXISTS `users` (
   `password` varchar(32) NOT NULL DEFAULT 'password',
   `acc_user_id` int(32) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `datetime_joined`, `username`, `password`, `acc_user_id`) VALUES
-(1, '2015-06-22 16:16:08', 'Mark Oates', 'password', 1146),
-(2, '2015-06-22 22:12:42', 'edgar', 'edgar_pass', 8592);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+(1, '2015-06-22 22:12:42', 'alex', 'password', 1146);
